fd4f8f50ae4571407a59e713fc5ed4b367af5e83
HAL420 <HAL420@users.noreply.github.com>
2014-09-03 21:59:48 +0400

    v0.01

---
+++ DasItManeCmd/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DasItManeCmd
+{
+    class Program
+    {
+        static void DoIt() {
+             string strIntro =
+"  ______            _____ _    ___  ___\n" +
+"  |  _  \\          |_   _| |   |  \\/  |\n" +
+"  | | | |__ _ ___    | | | |_  | .  . | __ _ _ __   ___ \n" +
+"  | | | / _` / __|   | | | __| | |\\/| |/ _` | '_ \\ / _ \\ \n" +
+"  | |/ / (_| \\__ \\  _| |_| |_  | |  | | (_| | | | |  __/ \n" +
+"  |___/ \\__,_|___/  \\___/ \\__| \\_|  |_/\\__,_|_| |_|\\___|\n" +
+"            [ 420 Blaze It Simulator 2014 ]";
+            string strQuestion = "You see a dank ass blunt chilaxing on the edge of your table.\nDo you toke it? [Y/N]";
+            string answer;
+            ConsoleKeyInfo cki;
+            string strResultYes = "- Yeah.\nDas it mane.";
+            string strResultNo = "- Nah.\nWow. Gay.";
+            string strUnknown = "Come again?";
+            string strRestart = "Restart? [Y/N]";
+
+            Console.Clear();
+            Console.WriteLine("\n" + strIntro + "\n");
+            Console.WriteLine(strQuestion);
+            do
+            {
+                answer = Console.ReadKey(true).Key.ToString();
+                if (answer == "Y")
+                {
+                    Console.WriteLine(strResultYes);
+                    break;
+                }
+                else if (answer == "N")
+                {
+                    Console.WriteLine(strResultNo);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(strUnknown);
+                }
+            } while (true);
+            Console.ReadKey(true);
+            Console.WriteLine("\n" + strRestart);
+            answer = Console.ReadKey(true).Key.ToString();
+            if (answer == "Y")
+            {
+                DoIt();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Das It Mane: 420 Blaze It Simulator 2014";
+            DoIt();
+        }
+    }
+}
---
+++ DasItManeCmd/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DasItManeCmd")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HAL 420")]
+[assembly: AssemblyProduct("DasItManeCmd")]
+[assembly: AssemblyCopyright("Copyleft © GNU/GPL")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29895214-1439-4a34-ac5b-664d15c8bfd9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
